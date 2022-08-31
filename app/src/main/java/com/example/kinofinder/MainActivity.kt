c561c8bddae1ede2062514b5dee4464ca55659b4
OrlovDmitriy <dima.orlov.777@gmail.com>
2022-08-31 11:36:15 +0500

    исправлены названия верхнего и нижнего меню, добавлены кнопки, добавлены картинки в верхнее меню

--- app/src/main/java/com/example/kinofinder/MainActivity.kt
+++ app/src/main/java/com/example/kinofinder/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.kinofinder
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initMenuButtons()
+
+    }
+
+    fun initMenuButtons() {
+        val menu = findViewById<Button>(R.id.btn_menu)
+        val favorites = findViewById<Button>(R.id.btn_favorites)
+        val watchLater = findViewById<Button>(R.id.btn_watch_later)
+        val selections = findViewById<Button>(R.id.btn_selections)
+        val settings = findViewById<Button>(R.id.btn_settings)
+
+        menu.setOnClickListener{
+            Toast.makeText(this, "Меню", Toast.LENGTH_LONG).show()
+        }
+
+        favorites.setOnClickListener{
+            Toast.makeText(this, "Избранное", Toast.LENGTH_LONG).show()
+        }
+
+        watchLater.setOnClickListener{
+            Toast.makeText(this, "Смотреть позже", Toast.LENGTH_LONG).show()
+        }
+
+        selections.setOnClickListener{
+            Toast.makeText(this, "Подборки", Toast.LENGTH_LONG).show()
+        }
+
+        settings.setOnClickListener{
+            Toast.makeText(this, "Настройки", Toast.LENGTH_LONG).show()
+        }
     }
 }
